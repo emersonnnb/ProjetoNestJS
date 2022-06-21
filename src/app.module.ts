12fccf7ae8134ed49da2da9c1f64fc4a26e87769
Emerson <emersonnnb@hotmail.com>
2022-06-21 11:18:16 -0300

    Ajuste

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArtefatoController } from './artefato/artefato/artefato.controller';
+import { ApiService } from './service/api/api.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ArtefatoController],
+  providers: [AppService, ApiService],
 })
-export class AppModule {}
+export class AppModule { }
