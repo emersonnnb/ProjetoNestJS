eb779523618db46aeb426df1c9d42577368e0908
Emerson <emersonnnb@hotmail.com>
2022-06-25 12:56:36 -0300

    validação do createArtefato e updateArtefato

--- src/Entities/artefato.entity.ts
+++ src/Entities/artefato.entity.ts
@@ -1,13 +1,37 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity() // decorator utilizado para mapear as tabelas do banco
 export class Artefato {
+
+    @PrimaryGeneratedColumn()  // cria uma PRIMARY KEY AUTO_INCREMENT 
     id: number;
+
+    @Column()                // Criar uma coluna no banco de dados
     situacao: string;
+
+    @Column()
     classe: string;
+
+    @Column()
     tipo: string;
+
+    @Column()
     quantidade: number;
+
+    @Column()
     valorEstimado: string;
+
+    @Column()
     apresentante: string;
+
+    @Column()
     proprietario: string;
+
+    @Column()
     descricao: string;
+
+    @Column()
     observacao: string;
 
 }
